eb98ad1ae6e412a4f8a9a7ed70fe7cac8a69d52e
Hammerknuden <finnjorg@mail.dk>
2024-11-19 12:57:13 +0100

    Update streamlit_app.py
    ) added

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ if year == '2024':
       #url = (r"https://1drv.ms/x/s!AkHtB4-z9Oc7gbVTAYTv_TQ3m5nMhg?e=Jle4TQ&nav=MTVfe0VFMzMxMjM2LUNFNzMtNDRDQS1COTY5LTRFQzFGMzg4N0E0N30")
       #df =pd.read_excel('url','book_simp')
       #df = pd.read_excel(r"https://1drv.ms/x/s!AkHtB4-z9Oc7gbVTAYTv_TQ3m5nMhg.book_simp")
-      df = pd.read_excel('2024 Booking 1_0.xlsx','book_simp'
+      df = pd.read_excel('2024 Booking 1_0.xlsx','book_simp')
       #(r"C:\Users\finnj\OneDrive\DELE MAPPE NAJA\HAMMERKNUDEN\BOOKING\2024 BOOKING 1_0.xlsx", 'book_simp')
       #df = pd.read_excel(https://1drv.ms/x/s!AkHtB4-z9Oc7gbVTAYTv_TQ3m5nMhg?e=Jle4TQ&nav=MTVfe0VFMzMxMjM2LUNFNzMtNDRDQS1COTY5LTRFQzFGMzg4N0E0N30
     if bruger == "naja":
